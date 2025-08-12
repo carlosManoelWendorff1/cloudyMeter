09070b0c7f877c188550701459753e7f0a7848a6
Carlos Manoel Wendorff <e-carlosw@weg.net>
2025-08-12 13:11:11 -0300

    feat(setup): initialize Next.js project with basic structure and configurations

---
+++ frontend/cloudy-meter/src/components/login-form.tsx
@@ -0,0 +1,95 @@
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { useForm } from "react-hook-form";
+
+type LoginFormValues = {
+  username: string;
+  password: string;
+};
+
+export function LoginForm({
+  className,
+  ...props
+}: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<LoginFormValues>();
+
+  const onSubmit = async (data: LoginFormValues) => {
+    console.log("Login data:", data);
+    // aqui você chama sua API ou backend
+  };
+
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card>
+        <CardHeader>
+          <CardTitle>Login to Organization</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="flex flex-col gap-6">
+              <div className="grid gap-3">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  placeholder="..."
+                  {...register("username", {
+                    required: "username is required",
+                  })}
+                />
+                {errors.username && (
+                  <span className="text-sm text-error">
+                    {errors.username.message}
+                  </span>
+                )}
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  placeholder="..."
+                  {...register("password", {
+                    required: "Password is required",
+                  })}
+                />
+                {errors.password && (
+                  <span className="text-sm text-error">
+                    {errors.password.message}
+                  </span>
+                )}
+              </div>
+              <div className="flex flex-col gap-3">
+                <Button
+                  type="submit"
+                  className="w-full"
+                  disabled={isSubmitting}
+                >
+                  {isSubmitting ? "Logging in..." : "Login"}
+                </Button>
+              </div>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
