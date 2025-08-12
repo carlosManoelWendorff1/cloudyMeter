dddee0572e08572d03209ca0d081425f9a91fc81
Carlos Manoel Wendorff <e-carlosw@weg.net>
2025-08-12 11:23:01 -0300

    feat(setup): initialize Spring Boot project with Maven

---
+++ backend/cloudyMeter/src/main/java/wendorff/cloudyMeter/CloudyMeterApplication.java
@@ -0,0 +1,13 @@
+package wendorff.cloudyMeter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudyMeterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudyMeterApplication.class, args);
+	}
+
+}
---
+++ backend/cloudyMeter/src/test/java/wendorff/cloudyMeter/CloudyMeterApplicationTests.java
@@ -0,0 +1,13 @@
+package wendorff.cloudyMeter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudyMeterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
